d0dae6fbf1b2fabeec34b0615aac39f683dc7cdb
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-25 21:38:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize (id:,name:,type:,db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name,type,db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type, db)
+      VALUES (?, ?);
+    SQL
+
+    db.execute(sql, [name,type])
+
+  end
+
+  def find
+  end
+
+
 end
